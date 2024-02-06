45857e30e03da0b3d9a8f0b6a0e04666e266a976
ktruong7 <ktruong7@uoregon.edu>
2024-02-06 13:47:56 -0800

    preproccess displays spectrogram plot, resizing is still problematic

--- Code/Preproccess.py
+++ Code/Preproccess.py
@@ -22,11 +22,13 @@ def LoadWav16kMono(filename):
     wav, sample_rate = tf.audio.decode_wav(file_contents, desired_channels=1)
     # Remove trailing axis
     wav = tf.squeeze(wav, axis=[-1])
-    # sample_rate = tf.cast(sample_rate, dtype=tf.int64)
 
+    # RESIZING STILL PROBLEMATIC
+    # sample_rate = tf.cast(sample_rate, dtype=tf.int64)
     # Convert to 16000 hertz, used to reduce the size of final audio
     # Depending on the audio files the WILL NEED TO BE FINE TUNED
     # wav = tfio.audio.resample(wav, rate_in=sample_rate, rate_out=16000)
+
     return wav
 
 
@@ -45,6 +47,26 @@ def PlotWav(input_wav):
     plt.show()
 
 
+"""
+preprocess - takes filepath and converts wav file to
+                spectrogram
+
+:arguments
+    filepath - wav file
+    label
+The resulting spectrogram will be displayed with matplotlib
+"""
+def preprocess(file_path, label):
+    wav = LoadWav16kMono(file_path)
+    wav = wav[:48000]
+    zero_padding = tf.zeros([48000] - tf.shape(wav), dtype=tf.float32)
+    wav = tf.concat([zero_padding, wav], 0)
+    spectrogram = tf.signal.stft(wav, frame_length=320, frame_step=32)
+    spectrogram = tf.abs(spectrogram)
+    spectrogram = tf.expand_dims(spectrogram, axis=2)
+    return spectrogram, label
+
+
 """ MAIN TEST LAUNCH"""
 # Change this to your own path to the dataset
 DATASET_PATH = 'c:/Users/redsl//PycharmProjects/pythonProject8/CS433_Project/KAGGLE/'
@@ -52,9 +74,15 @@ DATASET_PATH = 'c:/Users/redsl//PycharmProjects/pythonProject8/CS433_Project/KAG
 REAL_AUDIO = os.path.join(DATASET_PATH, 'AUDIO', 'REAL', 'biden-original.wav')
 FAKE_AUDIO = os.path.join(DATASET_PATH, 'AUDIO', 'FAKE', 'biden-to-linus.wav')
 
-wave = LoadWav16kMono(REAL_AUDIO)
-nwave = LoadWav16kMono(FAKE_AUDIO)
+# wave = LoadWav16kMono(REAL_AUDIO)
+# nwave = LoadWav16kMono(FAKE_AUDIO)
 
-plt.plot(nwave)
-plt.plot(wave)
+spectro, label = preprocess(REAL_AUDIO, 0.0)
+plt.figure(figsize=(30, 20))
+plt.imshow(tf.transpose(spectro)[0])
+plt.show()
+
+
+# plt.plot(nwave)
+# plt.plot(wave)
 plt.show()
